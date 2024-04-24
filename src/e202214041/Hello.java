a590105a9525d52a04693ac5be3b5b0d29cc8be1
nmmn1004 <jjj4120@gmail.com>
2024-04-24 13:16:08 +0900

    fifth

--- src/e202214041/Hello.java
+++ src/e202214041/Hello.java
@@ -1,7 +1,3 @@
 package e202214041;
 
-public class Hello {
-    public static void run() {
-        System.out.println("hello world");
-    }
-}
+public class Hello { public static void run() { System.out.println("hello world"); System.out.println("최준혁"); } }
