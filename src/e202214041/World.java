c41d1a3aac26bc5c06bbf2f62cf3a20f65ef8ed1
nmmn1004 <jjj4120@gmail.com>
2024-04-24 13:14:26 +0900

    fourth

--- src/e202214041/World.java
+++ src/e202214041/World.java
@@ -1,3 +1,3 @@
 package e202214041;
 
-public class World { public static void run() { System.out.println("안녕하세요"); } }
+public class World { public static void run() { System.out.println("안녕하세요"); System.out.println("최준혁"); } }
